53fe7b96307a627ee7f6346a4548ce2acbccc3e4
Igor Abílio Santana Santos <igorbilho@hotmail.com>
2016-02-11 23:45:46 -0300

    Finalizando part3 do tutorial.

---
+++ ContactsAngularJs/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using ContactsAngularJs.Helpers;
+using ContactsAngularJs.Models;
+using System.Web.Mvc;
+
+namespace ContactsAngularJs.Controllers
+{
+    public class ContactController : JsonController
+    {
+        private ContactContext ContactsDB = new ContactContext();
+
+        public ActionResult GetContacts()
+        {
+            var Contacts = ContactsDB.Contacts;
+            return Json(Contacts, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ ContactsAngularJs/Models/ContactContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContactsAngularJs.Models
+{
+    public partial class ContactContext : DbContext
+    {
+        public virtual DbSet<Contact> Contacts { get; set; }
+    }
+}
